1244dd963dc81108e2aedaeecfdc74055dee8f93
AzerBetaX <azer.betax@gmail.com>
2022-09-24 10:17:56 +0200

    Update TD4.cs

--- tds/TD4.cs
+++ tds/TD4.cs
@@ -107,18 +107,21 @@ public class TD4 : TDManager
         int compteur = 0;
         while (compteur != n)
         { 
-            if (compteurboucle % 2 == 1)
+            if (compteurboucle % 2 == 1 )
             {
-                
-
+                tab[compteur] = compteurboucle;
+                compteur++;
             }
 
-                
-                compteurboucle++;
+            compteurboucle++;
+               
         }
 
-
-
+    
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(tab[i]+" ");
+        }
 
 
     }
@@ -179,6 +182,7 @@ public class TD4 : TDManager
 
     }
 
+<<<<<<< HEAD
     public void Exercice10()
     {
         
@@ -192,6 +196,32 @@ public class TD4 : TDManager
             somme += tab[i];
         
         Console.WriteLine("La moyenne des " + nb + " éléments est ==> " + (float)((float)somme/(float)nb));
+=======
+    public int[] GenererTableauAleratoire(int taille, int lowerbound , int uppderbound)
+    {
+        Random rand = new Random();
+        int[] tab = new int[taille];
+        for (int i = 0; i < taille; i++)
+        {
+            int val = rand.Next(lowerbound, uppderbound);
+            tab[i] = val;
+
+        }
+
+        return tab;
+
+    }
+
+    public float Moyenne(int[] tab)
+    {
+        float somme = 0F;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            somme += tab[i];
+        }
+
+        return somme / tab.Length;
+>>>>>>> a70b5abde886e36b08280a790c26489f772b75b4
     }
     
     
@@ -282,6 +312,7 @@ public class TD4 : TDManager
 
     public int[,] ProduitMatriciel(int[,] mat1, int[,] mat2)
     {
+<<<<<<< HEAD
 
         int[,] mat3 = new int[mat1.GetLength(0), mat2.GetLength(1)];
 
@@ -294,6 +325,25 @@ public class TD4 : TDManager
     }
 
   
+=======
+        for (int i = 0; i < mat1.GetLength(0); i++)
+        {
+            for (int j = 0; j < mat1.GetLength(1); j++)
+            {
+                
+                
+                
+            }
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+>>>>>>> a70b5abde886e36b08280a790c26489f772b75b4
    
     
     
